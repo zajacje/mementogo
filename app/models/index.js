1fdc88f9333ee03094241bda67b6f3cec5276cae
Jennifer Zajac <zajac.je@northeastern.edu>
2021-09-25 12:56:19 -0400

    site now says mementoGo

--- app/models/index.js
+++ app/models/index.js
@@ -20,7 +20,8 @@ fs.readdirSync(__dirname)
     return file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js";
   })
   .forEach(file => {
-    const model = sequelize["import"](path.join(__dirname, file));
+    //const model = sequelize["import"](path.join(__dirname, file));
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
     db[model.name] = model;
   });
 
